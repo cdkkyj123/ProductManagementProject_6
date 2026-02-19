977c1ef8676fec5b7bf6c91fe3fcbca89fa66cba
SokLee <cdkkyj123@gmail.com>
2026-02-19 15:59:47 +0900

    Delete src/main/java/com/example/productmanagementproject_6/cart directory
    ㅇㄻㄴ

--- src/main/java/com/example/productmanagementproject_6/cart/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.productmanagementproject_6.cart.repository;
-
-import com.example.productmanagementproject_6.cart.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-
-}
