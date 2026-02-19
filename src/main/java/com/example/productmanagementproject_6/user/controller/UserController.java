e4b5f5e6e0bb3bde3791d2fdb380732e2bcc474d
SeungHyeonLee <leesh27955@gmail.com>
2026-02-19 14:55:38 +0900

    Feat : User 구성요소 생성 완료 (#6)

---
+++ src/main/java/com/example/productmanagementproject_6/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.productmanagementproject_6.user.controller;
+
+import com.example.productmanagementproject_6.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+}
