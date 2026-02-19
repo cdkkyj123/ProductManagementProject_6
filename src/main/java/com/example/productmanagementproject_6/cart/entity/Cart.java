c6764cc1b0ba5a71b7299dede7944d0c9378656f
SeungHyeonLee <leesh27955@gmail.com>
2026-02-19 20:05:53 +0900

    Feature/tmd (#18)
    * Feat : User 구성요소 생성 완료
    
    * Feat : Cart 구성요소 생성 완료 (#8)
    
    Co-authored-by: growingtree-code <tjsruddks12@gmail.com>
    
    * Feat : order 구성요소 생성 완료 (#5)
    
    * Dev (#3)
    
    * test branch
    
    * Refactor : Auditing 어노테이션 추가
    
    ---------
    
    Co-authored-by: jasmin <cjm98@smartel.co.kr>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    
    * Feat : order 구성요소 생성 완료
    
    ---------
    
    Co-authored-by: SokLee <cdkkyj123@gmail.com>
    
    * Feat : Porduct 구성요소 생성 완료 (#7)
    
    Co-authored-by: pposong0625 <posong0625@gmail.com>
    
    * Feat : 연관관계 활성화 (#10)
    
    * Dev (#3)
    
    * test branch
    
    * Refactor : Auditing 어노테이션 추가
    
    ---------
    
    Co-authored-by: jasmin <cjm98@smartel.co.kr>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    
    * Feat : Cart 구성요소 생성 완료 (#8)
    
    Co-authored-by: growingtree-code <tjsruddks12@gmail.com>
    
    * Feat : 연관관계 활성화
    
    ---------
    
    Co-authored-by: jasmin <cjm98@smartel.co.kr>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    Co-authored-by: grown-tree <92420848+grown-tree@users.noreply.github.com>
    Co-authored-by: growingtree-code <tjsruddks12@gmail.com>
    # Conflicts:
    #	src/main/java/com/example/productmanagementproject_6/cart/entity/Cart.java
    
    * Feat : order 구성요소 생성 완료 (#14)
    
    * Feat : order 구성요소 생성 완료
    
    ---------
    
    Co-authored-by: jasmin <cjm98@smartel.co.kr>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    Co-authored-by: SokLee <cdkkyj123@gmail.com>
    
    * Fix : Conflict error fix (#16)
    
    * Fix : Conflict error fix
    
    ---------
    
    Co-authored-by: jasmin <cjm98@smartel.co.kr>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    Co-authored-by: grown-tree <92420848+grown-tree@users.noreply.github.com>
    Co-authored-by: growingtree-code <tjsruddks12@gmail.com>
    
    ---------
    
    Co-authored-by: grown-tree <92420848+grown-tree@users.noreply.github.com>
    Co-authored-by: growingtree-code <tjsruddks12@gmail.com>
    Co-authored-by: jasmin <a01095567997@gmail.com>
    Co-authored-by: SokLee <cdkkyj123@gmail.com>
    Co-authored-by: pposong <okokok_1@naver.com>
    Co-authored-by: pposong0625 <posong0625@gmail.com>
    Co-authored-by: jasmin <cjm98@smartel.co.kr>

--- src/main/java/com/example/productmanagementproject_6/cart/entity/Cart.java
+++ src/main/java/com/example/productmanagementproject_6/cart/entity/Cart.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
